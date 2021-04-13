8d2be7a1120dfbeaa8b4e2f78949291bb37fa43d
loras26 <nyambegugu@gmail.com>
2021-04-13 20:06:41 +0200

    the greet function

---
+++ greet.test.js
@@ -0,0 +1,8 @@
+describe('The test for the greet function' , function(){
+    it('This function should return : "Hello, pupu"' , function(){
+        assert.equal("Hello, pupu", greet("pupu"));
+
+        //assert.deepEqual([2,2],[2,2]);
+    });
+
+});
