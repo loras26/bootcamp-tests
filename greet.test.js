63c725319d0db49a7734a8b81e4168caa4e3aa6f
loras26 <nyambegugu@gmail.com>
2021-04-14 10:00:20 +0200

    updated files

--- greet.test.js
+++ greet.test.js
@@ -1,5 +1,5 @@
 describe('The test for the greet function' , function(){
-    it('This function should return : "Hello, pupu"' , function(){
+    it('This function should return "Hello, pupu" when i pass the name pupu' , function(){
         assert.equal("Hello, pupu", greet("pupu"));
 
         //assert.deepEqual([2,2],[2,2]);
