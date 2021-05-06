7d18786a46e51da2a7331121a0de23cf59b498e7
loras26 <nyambegugu@gmail.com>
2021-05-06 23:21:52 +0200

    modified file

--- wordGame.test.js
+++ wordGame.test.js
@@ -1,21 +1,14 @@
 describe('The test for the wordGame function' , function(){
-    it('This function takes in a comma separated string of calls made and smss sent and it should return the total phone bill' , function(){
+    it('This function should return the longest word in the string passed to it' , function(){
         assert.equal("shipyard", longestWord("The dog jumped over the shipyard fences"));
-        assert.equal("the", shortestWord("The dog jumped over the shipyard fences"));
-        assert.equal("32", wordLengths("The dog jumped over the shipyard fence"));
-
         
     });
-    it('This function takes in a comma separated string of calls made and smss sent and it should return the total phone bill' , function(){
-        assert.equal("shipyard", longestWord("The dog jumped over the shipyard fences"));
+    it('This function should return the shortest word in the string passed to it' , function(){
         assert.equal("the", shortestWord("The dog jumped over the shipyard fences"));
-        assert.equal("32", wordLengths("The dog jumped over the shipyard fence"));
-
         
     });
-    it('This function takes in a comma separated string of calls made and smss sent and it should return the total phone bill' , function(){
-        assert.equal("shipyard", longestWord("The dog jumped over the shipyard fences"));
-        assert.equal("the", shortestWord("The dog jumped over the shipyard fences"));
+    it('This function should return the total length of the characters in a string passed to it.' , function(){
+        
         assert.equal("32", wordLengths("The dog jumped over the shipyard fence"));
 
         
